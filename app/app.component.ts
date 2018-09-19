651be259f5fef0c02786ddcb2cd14e2e560dfe84
DMoreno06 <mordiana1506@gmail.com>
2018-09-19 14:04:26 -0500

    first commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,23 +13,18 @@ export class AppComponent {
   title = 'myJson';
   parametro = '';
   result: any = [];
-  btnResult: string = 'Generar';
-  //APIUrl: string = 'http://10.5.0.120:8983/solr/core2/select?indent=on&q=' + this.param + '&wt=json';
-
-  APIUrl: string = 'http://10.5.0.120:8983/solr/core2/select?indent=on';
+  btnResult = 'Generar';
+  APIUrl = 'http://10.5.0.120:8983/solr/core2/select?indent=on';
 
   constructor(private json: JsonService) {  }
-  // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit() {
-  // tslint:disable-next-line:quotemark
-  this.json.verJson(this.APIUrl + "&q=' + doc14  + '&wt=json").
+  this.json.verJson(this.APIUrl + '&q=' + this.parametro + '&wt=json').
   subscribe(val => (this.result = val), error => (console.log('!UPS tenemos un error: ', error)));
   }
 generateData(param): void {
   this.parametro = param;
   this.json
-    // tslint:disable-next-line:quotemark
-    .verJson(this.APIUrl + "&q=' + doc14+ '&wt=json")
+    .verJson(this.APIUrl + '&q=' + this.parametro + '&wt=json')
     .subscribe(
     resultQuestion => (this.result = resultQuestion),
     error => (console.log('Ups! we have an error: ', error))
